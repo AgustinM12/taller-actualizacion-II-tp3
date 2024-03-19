e6144d6e963d951b7de4ec871d0dad66ff8c75dd
AgustinM12 <agustinmazza12345@gmail.com>
2024-03-19 16:52:38 -0300

    se empezo a desarrollar la funcionalidad de crear las matrices

---
+++ servidor/app.js
@@ -0,0 +1,11 @@
+import express, { Router } from "express"
+
+const app = express()
+
+const calculatorRoute = Router()
+
+calculatorRoute.get("/", )
+
+app.listen(5000, () => {
+    log("Servidor corridendo en localhost:", 5000)
+})
